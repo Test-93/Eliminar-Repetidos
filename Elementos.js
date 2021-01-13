e9bac2841f7bfbec6e6eab365b914ca61fdbddb1
JuanJose93 <rossi.juanjose.93@gmail.com>
2021-01-13 01:12:47 -0300

    Elementos Faltantes Agregados( Parametro). Llamado de la funcion. Informe de resultados.

--- Elementos.js
+++ Elementos.js
@@ -1,9 +1,7 @@
 let arrMinToMax=[1,1,1,1,2,2,3,3,4];
 let arrMixed=[1,2,1,1,3,4,3,1,2]
 
-
-function repMinToMax(){
-  let arr=arrMinToMax;
+function RepeMinToMax(arr){
   let auxArr=[];
   for (let i = 0; i < arr.length; i++) {
     if (arr[i]!==arr[i+1]) {
@@ -11,5 +9,7 @@ function repMinToMax(){
     }
   }
   //document.write(auxArr);
-  return((auxArr))
+  return((auxArr));
 }
+
+document.write(RepeMinToMax(arrMinToMax));
